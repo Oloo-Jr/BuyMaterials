ee6c5929e42dd29daa51ecb24d7449d2041e1165
LeonardMuia <leonardmuia@outlook.com>
2024-05-13 09:18:56 +0300

    added primereact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Screens/SignIn/SignIn';
 import AddProduct from './Screens/AddProduct/AddProduct';
 
+import 'primeicons/primeicons.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 
 function App() {
   return (
