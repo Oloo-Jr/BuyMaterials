43f5c0262609fc292774815519077b4dc2a7bdb4
LeonardMuia <leonardmuia@outlook.com>
2024-05-13 09:26:43 +0300

    configured react libraries

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Screens/SignIn/SignIn';
 import AddProduct from './Screens/AddProduct/AddProduct';
-
 import 'primeicons/primeicons.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-
 
 function App() {
   return (
